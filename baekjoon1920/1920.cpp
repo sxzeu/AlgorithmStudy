89ed80ddacd065e33cfd28289262c59fb4f16529
sxzeu <sxzeu@naver.com>
2021-10-06 23:48:28 +0900

    Create 1920.cpp

---
+++ baekjoon1920/1920.cpp
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+int main()
+{
+	// 입력
+	vector<int> v1;
+	// 체크
+	vector<int>v2;
+
+	int n, m;
+	int input;
+
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> input;
+		v1.push_back(input);
+	}
+	cin >> m;
+	for (int i = 0; i < m; i++)
+	{
+		cin >> input;
+		v2.push_back(input);
+	}
+
+	for (int i = 0; i < m ; i++) 
+	{
+		if (binary_search(v1.begin(), v1.end(), v2[i])) cout << "1" <<"\n";
+		else cout << "0" << "\n";
+	}
+	
+}

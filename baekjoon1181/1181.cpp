4e5d14a46bdb8d7e362ee08f38cab32acb49eebb
sxzeu <sxzeu@naver.com>
2021-10-11 22:24:02 +0900

    Update 1181.cpp

--- baekjoon1181/1181.cpp
+++ baekjoon1181/1181.cpp
@@ -1,33 +1,37 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
 bool compare(string a, string b) {
-	if (a.size() == b.size()) return a < b;
-	else return a.size() < b.size();
+    if (a.size() == b.size()) return a < b;
+    else return a.size() < b.size();
 }
 
 int main() {
 
-	vector <string> input;
-	int n;
-	cin >> n;
-	string tmp;
+    vector <string> input;
 
-	for (int i = 0; i < n; i++) {
-		string str;
-		cin >> str;
-		input.push_back(str);
-	}
+    string str;
+    string tmp;
 
-	sort(input.begin(), input.end(), compare);
+    int n;
+    cin >> n;
 
-	for (int i = 0; i < n; i++) {
-		if (tmp == input[i]) continue;
-		tmp = input[i];
-		cout << input[i] << '\n';
-	}
-	return 0;
+
+    for (int i = 0; i < n; i++) {
+        cin >> str;
+        input.push_back(str);
+    }
+
+    sort(input.begin(), input.end(), compare);
+
+    for (int i = 0; i < n; i++) {
+        if (tmp == input[i]) continue;
+        tmp = input[i];
+        cout << input[i] << '\n';
+    }
+
+    return 0;
 }

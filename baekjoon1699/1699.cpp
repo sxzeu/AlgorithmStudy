b4bd683e9fd3c390cb16958e3498381dae69da5f
sxzeu <sxzeu@naver.com>
2021-10-21 11:10:04 +0900

    Update 1699.cpp

--- baekjoon1699/1699.cpp
+++ baekjoon1699/1699.cpp
@@ -1,4 +1,3 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int dp[100001];
@@ -14,16 +13,13 @@ int main()
 	int n = 0;
 	scanf("%d", &n);
 
-	for (int i = 0; i <= n; i++)
-		dp[i] = i;
+	for (int i = 0; i <= n; i++) dp[i] = i;
 
-	for (int i = 2; i <= n; i++) {
+	for (int i = 4; i <= n; i++) {
 		for (int j = 2; j * j <= i; j++) {
-
 			dp[i] = min(dp[i], dp[i - j * j] + 1);
 		}
 	}
-
 	printf("%d\n", dp[n]);
 	return 0;
 }

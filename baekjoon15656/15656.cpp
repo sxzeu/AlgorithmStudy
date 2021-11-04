5e2add9175690ab34aa91b9700c6fc72b20a6272
sxzeu <sxzeu@naver.com>
2021-11-04 22:04:56 +0900

    Create 15656.cpp

---
+++ baekjoon15656/15656.cpp
@@ -0,0 +1,50 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool visited[8] = { 0, };
+int list[8] = { 0, };
+vector<int> v;
+int m, n;
+
+void dfs(int num)
+{
+	if (num == n)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			cout << list[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+
+	else
+	{
+		for (int i = 0; i < m; i++)
+		{
+			visited[i] = true;
+			list[num] = v[i];
+			dfs(num + 1);
+			visited[i] = false;
+		}
+	}
+}
+
+
+int main()
+{
+	cin >> m >> n;
+	
+	int temp; 
+	for (int i = 0; i < m; i++)
+	{
+		cin >> temp;
+		v.push_back(temp);
+	}
+	sort(v.begin(), v.end());
+	dfs(0);
+}

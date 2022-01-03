e15703ca2924ae5a59ea02505a4c1d203ac1de75
sxzeu <sxzeu@naver.com>
2022-01-03 22:39:27 +0900

    Create 14889.cpp

---
+++ baekjoon14889/14889.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+int ans = 1000000000; // 10¾ï
+
+void dfs(int x, int next) 
+{
+
+
+}
+
+int main()
+{
+	cin >> n;
+
+}

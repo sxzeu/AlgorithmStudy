ac68346f45b294f4f08f45a727c98aa62aa3afc9
sxzeu <63103601+sxzeu@users.noreply.github.com>
2022-01-04 13:16:14 +0900

    Update 14889.cpp

--- baekjoon14889/14889.cpp
+++ baekjoon14889/14889.cpp
@@ -4,17 +4,46 @@
 
 using namespace std;
 
-int n;
-int ans = 1000000000; // 10억
+int n; 
 
-void dfs(int x, int next) 
+int input[21][21] = { -1, };
+bool visited[21]= { 0, };
+
+int count = 0;
+int ans = 1000000;
+
+void dfs(int x, int start)
 {
+	if (x == n / 2)
+	{
+		int start = 0, link = 0;
+		for (int i = 1; i <= n; i++){
+			for (int j = 1; j <= n; j++){
+				if (visited[i]&& visited[j]) start += input[i][j];
+				if (!visited[i]&& !visited[j]) link += input[i][j];
+			}
+		}
+		int num = abs(start - link);
+		if (ans > num) ans = num; //최솟값 기록
+	}
 
-
+	for (int i = start; i < n; i++){
+		if (!visited[i]) {
+			visited[i] = 1;
+			dfs(x + 1, i + 1);
+			visited[i] = 0;
+		}
+	}
 }
 
 int main()
 {
-	cin >> n;
+	cin >> n; 
+	
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j <= n; j++) cin >> input[i][j];
 
+	dfs(0, 1);
+
+	cout << ans;
 }
